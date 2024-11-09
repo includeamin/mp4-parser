ab60b1aa09010fc416ad53418c84a632d4a840db
Amin Jamal <a.jamal@greyrook.com>
2024-11-09 21:00:43 +0100

    feat: add tests

--- tests/mp4_parser.rs
+++ tests/mp4_parser.rs
@@ -6,7 +6,13 @@ mod tests {
     fn test_parse_mp4() {
         let file = fs::read("tests/testdata/input.mp4").unwrap();
         let mp4 = mp4_parser::MP4Parser::from_buf(&file);
-        println!("{:?}", mp4);
-        assert_eq!(mp4.ftyp.unwrap().major_brand(), "mp42")
+
+        let ftyp = mp4.ftyp.unwrap();
+        assert_eq!(ftyp.major_brand(), "mp42");
+        assert_eq!(ftyp.minor_version(), 0);
+        assert_eq!(
+            ftyp.compatible_brands(),
+            vec!["mp42", "mp41", "isom", "avc1"]
+        )
     }
 }
